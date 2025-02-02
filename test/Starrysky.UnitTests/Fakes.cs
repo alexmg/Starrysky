38fd01311186a04350cd03b8f1d14b14d660c3fb
alexmg <alex.meyergleaves@gmail.com>
2025-02-02 22:37:53 +1000

    style: Add language injections to JSON strings

--- test/Starrysky.UnitTests/Fakes.cs
+++ test/Starrysky.UnitTests/Fakes.cs
@@ -9,24 +9,26 @@ public static class Fakes
     public static Repository CreateRepository(string name = "cool-lib", string owner = "dudes") =>
         new SimpleJsonSerializer()
             .Deserialize<Repository>(
+                //language=JSON
                 $$"""
                   {
-                      "id": 123,
-                      "name": "{{name}}",
-                      "description": "{{name}} is a cool repo",
-                      "html_url": "https://github.com/{{owner}}/{{name}}",
-                      "owner": {
-                          "login": "{{owner}}"
-                      }
+                    "id": 123,
+                    "name": "{{name}}",
+                    "description": "{{name}} is a cool repo",
+                    "html_url": "https://github.com/{{owner}}/{{name}}",
+                    "owner": {
+                      "login": "{{owner}}"
+                    }
                   }
                   """);
 
     public static User CreateUser(string login = "username") =>
         new SimpleJsonSerializer()
             .Deserialize<User>(
+                //language=JSON
                 $$"""
                   {
-                      "login": "{{login}}"
+                    "login": "{{login}}"
                   }
                   """);
 
